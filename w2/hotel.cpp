500a870731e272f89f253618e15955503bff8b3b
N-911 <nikolayenkoserhiy@gmail.com>
2020-08-07 16:54:58 +0300

    hotel

--- w2/hotel.cpp
+++ w2/hotel.cpp
@@ -3,17 +3,17 @@
 #include <vector>
 #include <iomanip>
 #include <deque>
+#include <queue>
 
 using namespace std;
 
-
 struct s_hotel {
 public :
-    s_hotel(int _time, int _client_id, size_t _room_count) : time(_time),
+    s_hotel(int64_t _time, int _client_id, size_t _room_count) : time(_time),
                         client_id(_client_id), room_count(_room_count) {}
     s_hotel() {}
-    int time;
-//    string hotel_name;
+
+    int64_t time;
     int client_id;
     size_t room_count;
 };
@@ -22,9 +22,10 @@ class Hotel {
 public:
     Hotel() = default;
 
-    void Book(int time, string hotel_name, int client_id, size_t room_count) {
-        all_hotels[hotel_name].push_back(s_hotel({time, client_id, room_count}));
+    void Book(int64_t time, string hotel_name, int client_id, size_t room_count) {
+        all_hotels[hotel_name].push({time, client_id, room_count});
         rooms_count[hotel_name] += room_count;
+        ++clients_count[hotel_name][client_id];
         current_time = time;
     };
 
@@ -34,9 +35,9 @@ public:
 
     int Clients(const string& hotel_name) {
         Update_hotels(hotel_name);
-
-    return 0;
+        return clients_count[hotel_name].size();
     }
+
     int Rooms(const string& hotel_name) {
         Update_hotels(hotel_name);
         return rooms_count[hotel_name];
@@ -44,17 +45,25 @@ public:
 
 private:
     int64_t current_time = 0;
-    map<string, deque<s_hotel>> all_hotels;
+    map<string, queue<s_hotel>> all_hotels;
     map<string, int> rooms_count;  // key->hotel_name   value->struct{time, hotel_name, cl_id, room_c}
+    map<string, map<int, int>> clients_count;
 
-    void Update_hotels(string hotel_name) {
+    void Update_hotels(const string& hotel_name) {
     int cur_t = Get_current_time();
 
-    while (!all_hotels[hotel_name].empty() && all_hotels[hotel_name].front().time <= current_time - 86400) {
-        int temp = all_hotels[hotel_name].front().room_count;
-        all_hotels[hotel_name].pop_front();
-        rooms_count[hotel_name] -=temp;
+        while (!all_hotels[hotel_name].empty() && all_hotels[hotel_name].front().time <= current_time - 86400) {
+            int temp_room = all_hotels[hotel_name].front().room_count;
+            int temp_client = all_hotels[hotel_name].front().client_id;
 
+            all_hotels[hotel_name].pop();
+            rooms_count[hotel_name] -= temp_room;
+
+            if (clients_count[hotel_name][temp_client] == 1) {
+                clients_count[hotel_name].erase(temp_client);
+            } else {
+                --clients_count[hotel_name][temp_client];
+            }
         }
     }
 };
@@ -90,7 +99,7 @@ int main() {
         } else if (query_type == "ROOMS") {
             string hotel_name;
             cin >> hotel_name;
-            cout << hotels.Rooms(hotel_name);
+            cout << hotels.Rooms(hotel_name) << '\n';
         }
     }
 
@@ -98,19 +107,71 @@ int main() {
 }
 
 
-//        auto it_clients =
-//        auto res = *it;
-//        cout << res.time << ' ' << res.hotel_name << endl;
-//        return all_hotels[hotel_name].end() - it;
-
-//    auto it = lower_bound(all_hotels[hotel_name].begin(), all_hotels[hotel_name].end(), s_hotel(),
-//                          [cur_t](const s_hotel& lhs, const s_hotel& rhs){
-//                              return lhs.time < (cur_t - 86400); } );
-//    vector<s_hotel> temp = {it, all_hotels[hotel_name].end()};
-
 /*
- * bool Cmp_time(const int& time_1, const int& time_2) {
-    return time_1 < time_2;
+ * class HotelManager {
+public:
+  void Book(int64_t time, const string& hotel_name,
+            int client_id, int room_count) {
+    current_time_ = time;
+    hotels_[hotel_name].Book({time, client_id, room_count});
+  }
+  int ComputeClientCount(const string& hotel_name) {
+    return hotels_[hotel_name].ComputeClientCount(current_time_);
+  }
+  int ComputeRoomCount(const string& hotel_name) {
+    return hotels_[hotel_name].ComputeRoomCount(current_time_);
+  }
 
-}
+private:
+  struct Booking {
+    int64_t time;
+    int client_id;
+    int room_count;
+  };
+
+  class HotelInfo {
+  public:
+    void Book(const Booking& booking) {
+      last_bookings_.push(booking);
+      room_count_ += booking.room_count;
+      ++client_booking_counts_[booking.client_id];
+    }
+    int ComputeClientCount(int64_t current_time) {
+      RemoveOldBookings(current_time);
+      return client_booking_counts_.size();
+    }
+    int ComputeRoomCount(int64_t current_time) {
+      RemoveOldBookings(current_time);
+      return room_count_;
+    }
+  private:
+    static const int TIME_WINDOW_SIZE = 86400;
+    queue<Booking> last_bookings_;
+    int room_count_ = 0;
+    map<int, int> client_booking_counts_;
+
+    void PopBooking() {
+      const Booking& booking = last_bookings_.front();
+      room_count_ -= booking.room_count;
+      const auto client_stat_it =
+          client_booking_counts_.find(booking.client_id);
+      if (--client_stat_it->second == 0) {
+        client_booking_counts_.erase(client_stat_it);
+      }
+      last_bookings_.pop();
+    }
+    void RemoveOldBookings(int64_t current_time) {
+      while (
+          !last_bookings_.empty()
+          && last_bookings_.front().time <= current_time - TIME_WINDOW_SIZE
+      ) {
+        PopBooking();
+      }
+    }
+  };
+
+  int64_t current_time_ = 0;
+  map<string, HotelInfo> hotels_;
+};
  */
+
