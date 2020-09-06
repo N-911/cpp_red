a20b52dea049e0ee0722a80f34dedf50ed229723
MSM <nikolayenkoserhiy@gmail.com>
2020-09-07 00:06:13 +0300

    blog

--- w5/explore_key_words.cpp
+++ w5/explore_key_words.cpp
@@ -1,11 +1,17 @@
 #include "test_runner.h"
 #include "profile.h"
 
-#include <functional>
+#include <algorithm>
 #include <map>
+#include <fstream>
+#include <random>
+#include <vector>
 #include <string>
-#include <future>
 #include <sstream>
+#include <future>
+#include <iterator>
+#include <set>
+using namespace std;
 
 using namespace std;
 
@@ -19,6 +25,15 @@ struct Stats {
   }
 };
 
+set<string> ReadKeyWords(istream& is) {
+    return {istream_iterator<string>(is), istream_iterator<string>()};
+}
+
+vector<string> Split(const string& line) {
+    // http://en.cppreference.com/w/cpp/iterator/istream_iterator
+    istringstream line_splitter(line);
+    return {istream_iterator<string>(line_splitter), istream_iterator<string>()};
+}
 
 Stats ExploreLine(const set<string>& key_words, const string& line) {
     istringstream s_line(line);
@@ -65,7 +80,7 @@ Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
 
     for (string line; getline(input, line); ) {
         if (count_line == 5000 ) {
-            f_vector.push_back(async(ExploreLines, ref(key_words), part_lines));
+            f_vector.push_back(async(ExploreLines, ref(key_words), move(part_lines)));
             count_line = 0;
             part_lines.clear();
         }
@@ -73,7 +88,7 @@ Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
         count_line++;
     }
     if (count_line) {
-        f_vector.push_back(async(ExploreLines, ref(key_words), part_lines));
+        f_vector.push_back(async(ExploreLines, ref(key_words), move(part_lines)));
     }
 
     for (auto& elem : f_vector) {
@@ -83,37 +98,18 @@ Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
 }
 
 
- /*
-Stats ExploreLines(const vector<string>& lines, const set<string>& key_words) {
-    Stats stats;
-    for (const auto& line : lines) {
-        stats += ExploreLine(key_words, line);
-    }
-    return stats;
+void TestSplit() {
+    const vector<string> expected1 = {
+            "abc", "def", "ghi,", "!", "jklmnop-qrs,", "tuv"
+    };
+    ASSERT_EQUAL(Split("  abc def ghi, !  jklmnop-qrs, tuv"), expected1);
+
+    const vector<string> expected2 = {"a", "b"};
+    ASSERT_EQUAL(Split("a b      "), expected2);
+    ASSERT_EQUAL(Split(""), vector<string>());
 }
 
-Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
-    vector<vector<string>> lines(4);
 
-    int count = 0;
-    for (string line; getline(input, line); ) {
-        lines[count / 5000].push_back(line);
-        count++;
-    }
-
-    vector<future<Stats>> futures;
-    for (auto thread : lines) {
-        futures.push_back(async([=] { return ExploreLines(thread, key_words); }));
-    }
-    Stats word_frequences;
-    for (auto& future : futures) {
-        word_frequences += future.get();
-    }
-    return word_frequences;
-
-}
-
- */
 void TestBasic() {
   const set<string> key_words = {"yangle", "rocks", "sucks", "all"};
 
@@ -133,7 +129,71 @@ void TestBasic() {
   ASSERT_EQUAL(stats.word_frequences, expected);
 }
 
+void TestMtAgainstSt() {
+  {
+    std::default_random_engine rd(34);
+    std::uniform_int_distribution<char> char_gen('a', 'z');
+
+    auto random_word = [&](size_t len) {
+      string result(len, ' ');
+      std::generate(begin(result), end(result), [&] { return char_gen(rd); });
+      return result;
+    };
+
+    std::uniform_int_distribution<size_t> len_gen(5, 21);
+
+    vector<string> key_words(500);
+    for (auto& w : key_words) {
+      w = random_word(len_gen(rd));
+    }
+
+    ofstream out("key_words.txt");
+    for (const auto& w : key_words) {
+      out << w << ' ';
+    }
+
+    std::uniform_int_distribution<size_t> line_len_gen(5, 100);
+    std::uniform_int_distribution<int> word_choice(1, 100);
+
+    ofstream text_out("text.txt");
+    for (int line = 0; line < 25000; ++line) {
+      ostringstream line_out;
+      auto line_len = line_len_gen(rd);
+      for (size_t i = 0; i < line_len; ++i) {
+        if (word_choice(rd) < 20) {
+          std::uniform_int_distribution<size_t> word_index(0, key_words.size() - 1);
+          line_out << key_words[word_index(rd)];
+        } else {
+          line_out << random_word(len_gen(rd));
+        }
+        line_out << ' ';
+      }
+      text_out << line_out.str() << '\n';
+    }
+  }
+
+    ifstream key_words_input("key_words.txt");
+    const auto key_words_data = ReadKeyWords(key_words_input);
+    const set<string> key_words(key_words_data.begin(), key_words_data.end());
+
+    Stats st_stats, mt_stats;
+    {
+        ifstream text_input("text.txt");
+        LOG_DURATION("Single thread");
+        st_stats = ExploreKeyWordsSingleThread(key_words, text_input);
+    }
+    {
+        ifstream text_input("text.txt");
+        LOG_DURATION("Multi thread");
+        mt_stats = ExploreKeyWords(key_words, text_input);
+    }
+
+    ASSERT_EQUAL(st_stats.word_frequences, mt_stats.word_frequences);
+}
+
 int main() {
-  TestRunner tr;
-  RUN_TEST(tr, TestBasic);
+    TestRunner tr;
+    RUN_TEST(tr, TestSplit);
+    RUN_TEST(tr, TestBasic);
+    RUN_TEST(tr, TestMtAgainstSt);
 }
